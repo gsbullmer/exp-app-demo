f77b4e7bab511f4a795860ad012f42a3d43d0b92
Greg Bullmer <gsbullmer@gmail.com>
2016-02-11 14:17:17 -0600

    refactor: Change expense class to Expense

--- expapp/js/app.js
+++ expapp/js/app.js
@@ -1,7 +1,9 @@
 (function() {
   "use strict"
 
-  function expense(merchant, totalExpense, date, comments) {
+  var app = angular.module('expApp', []);
+
+  function Expense(merchant, totalExpense, date, comments) {
     this.merchant = merchant;
     this.totalExpense = totalExpense;
     this.date = date;
@@ -10,12 +12,10 @@
   }
 
   var lines = [
-    new expense("Adam", 1500, 312341643256, ""),
-    new expense("Bob", 2749.99, 312341643256, "This was expensive!"),
-    new expense("Chris", 2.49, 312341643256, "Energy drink for lunch.")
+    new Expense("Adam", 1500, 312341643256, ""),
+    new Expense("Bob", 2749.99, 312341643256, "This was expensive!"),
+    new Expense("Chris", 2.49, 312341643256, "Energy drink for lunch.")
   ];
-
-  var app = angular.module('expApp', []);
 
   app.controller('ExpenseController', function(){
     this.expenses = lines;
