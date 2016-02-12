f90e3e8472dcbd5a1b659e9f77edccf072606c75
Greg Bullmer <gsbullmer@gmail.com>
2016-02-12 14:01:09 -0600

    feat: Show message if no expenses, hide actions if not new, add new button

--- expapp/js/app.js
+++ expapp/js/app.js
@@ -3,22 +3,26 @@
 
   var app = angular.module('expApp', []);
 
-  function Expense(merchant, totalExpense, date, comments) {
+  function Expense(merchant, totalExpense, date, comments, status) {
     this.merchant = merchant;
     this.totalExpense = totalExpense;
     this.date = date;
     this.comments = comments;
-    this.status = "New";
+    this.status = status;
   }
 
   var lines = [
-    new Expense("Adam", 1500, 312341643256, ""),
-    new Expense("Bob", 2749.99, 312341643256, "This was expensive!"),
-    new Expense("Chris", 2.49, 312341643256, "Energy drink for lunch.")
+    new Expense("Adam", 1500, 312341643256, "", "New"),
+    new Expense("Bob", 2749.99, 312341643256, "This was expensive!", "New"),
+    new Expense("Chris", 2.49, 312341643256, "Energy drink for lunch.", "Reimbursed")
   ];
 
-  app.controller('ExpenseController', function(){
+  app.controller('ExpenseSheetController', function(){
     this.expenses = lines;
+
+    this.isNewExpense = function(expense) {
+      return expense.status === "New";
+    };
   });
 
 })();
