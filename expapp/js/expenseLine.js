24d125c8da201ce2b2c0a351935a9a0a2aae6629
Greg Bullmer <gsbullmer@gmail.com>
2016-02-13 16:08:13 -0600

    bug: Fix editing and saving of expenses

--- expapp/js/expenseLine.js
+++ expapp/js/expenseLine.js
@@ -3,14 +3,20 @@
 
     .controller('ExpenseController', ['$scope', function($scope) {
 
+      $scope.editExpense = function() {
+        $scope.expense.isEditing = true;
+        $scope.newExpense = angular.copy($scope.expense);
+      };
+
       $scope.updateExpense = function() {
         $scope.newExpense.isEditing = false;
         $scope.expense = angular.copy($scope.newExpense);
-      }
+      };
 
       $scope.resetExpense = function() {
+        $scope.expense.isEditing = false;
         $scope.newExpense = angular.copy($scope.expense);
-      }
+      };
 
       $scope.resetExpense();
     }])
