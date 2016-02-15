0abc714108c7113d56ca09e40819c212b59eca01
Greg Bullmer <gsbullmer@gmail.com>
2016-02-14 21:51:06 -0600

    feat: add validation to new expense

--- expapp/js/newExpenseForm.js
+++ expapp/js/newExpenseForm.js
@@ -1,19 +1,27 @@
 (function() {
   'useStrict'
-  
+
   /* global angular */
   angular.module('newExpenseForm', [])
 
     .controller('NewExpenseFormController', ['$scope', function($scope) {
       $scope.newExpense = {};
 
-      $scope.saveExpense = function(save) {
-        if (save) {
+      $scope.saveExpense = function() {
+        if ($scope.expenseForm.$valid) {
           $scope.expenses.push($scope.newExpense);
+          $scope.cancelExpense();
         }
+      };
+
+      $scope.cancelExpense = function() {
         $scope.newExpense = {};
+        $scope.newExpense.status = "New";
         $scope.hasNewExpense = false;
       };
+
+      // Cancel new expense and set status to "New"
+      $scope.cancelExpense();
 
     }])
   ;
